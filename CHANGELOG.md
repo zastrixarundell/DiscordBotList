22127d88d6a2d6e5a08368daeae9930fc4d0cfd0
zastrixarundell <zastrixarundell@gmail.com>
2020-04-14 22:49:30 +0200

    Updated version

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,7 @@
 # CHANGELOG
 
+## 0.1.1
+ * Deleted unneeded test.
+
 ## 0.1.0
  * Initial version distributed on Hex. 
--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DiscordBotList.MixProject do
   def project do
     [
       app: :discord_bot_list,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       build_embedded: Mix.env() == :prod,
