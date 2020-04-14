b70959caa533eee74a1b6a791edba5914672afd0
zastrixarundell <zastrixarundell@gmail.com>
2020-04-14 21:38:55 +0200

    Created POST method

---
+++ lib/discord_bot_list/struct/bot_stats.ex
@@ -0,0 +1,79 @@
+defmodule DiscordBotList.Struct.BotStats do
+  @moduledoc """
+  Module representing the bot stats from the DiscordBotList API.
+  """
+
+  defstruct [
+    :server_count,
+    :shards,
+    :shard_count,
+    :shard_id
+  ]
+
+  @type t() :: %__MODULE__{
+    server_count: integer(),
+    shards: [integer()],
+    shard_id: integer(),
+    shard_count: integer()
+  }
+
+  @typedoc "The amount of servers the bot is in. If an Array it acts like `shards`. Required if `shards` is not defined."
+  @type server_count :: integer() | [integer()]
+
+  @typedoc "The amount of servers the bot is in per shard. Not required unless `server_count` is not defined."
+  @type shards :: [integer()]
+
+  @typedoc "The zero-indexed id of the shard posting. Makes server_count set the shard specific server count."
+  @type shard_id :: integer()
+
+  @typedoc "The amount of shards the bot has."
+  @type shard_count :: integer()
+
+  def post_updated_data(config \\ []) do
+    alias DiscordBotList.State
+
+    token = Keyword.get(config, :token, State.get_token())
+    id = Keyword.get(config, :id, State.get_id())
+    struct = Keyword.get(config, :stats, State.get_id())
+
+    perform(token, id, struct, struct.shards, struct.server_count)
+  end
+
+  defp perform(_token, _id, _struct, shards, server_count) when is_nil(server_count) and is_nil(shards),
+    do: {:error, :counts_are_undefined}
+
+  defp perform(_token, _id, struct, _shards, _server_count) when is_struct(struct) != true,
+    do: {:error, :is_not_a_struct}
+
+
+  use DiscordBotList.Struct
+  @behaviour DiscordBotList.Struct
+
+  defp perform(token, id, struct, _shards, _server_count) do
+
+    struct =
+      struct
+      |> Map.from_struct()
+      |> Jason.encode!()
+
+    response =
+      "https://top.gg/api/bots/#{id}/stats"
+      |> HTTPoison.post!(struct, [{"Authorization", token}, {"Content-Type", "application/json"}])
+
+    case response do
+      %HTTPoison.Response{status_code: 200} ->
+        {:ok, nil}
+      error ->
+        error =
+          error.body
+          |> Jason.decode!()
+          |> Map.get("error")
+        {:error, error}
+    end
+  end
+
+  def generate_from_json_string(json_string) do
+    IO.write json_string
+  end
+
+end
